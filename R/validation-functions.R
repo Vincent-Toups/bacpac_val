a6cd769029036e3b70d21c8d5823e2963cdc5e0e
Patrick Ward <patrickjward@uky.edu>
2020-10-26 09:12:46 -0400

    fixing messed up merge

--- R/validation-functions.R
+++ R/validation-functions.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 column_is_textual <- function(column){
     s <- sprintf;
     function(state){
@@ -26,20 +25,19 @@ column_is_numeric <- function(column){
         if(class(the_col)=="numeric"){
             extend_state(state,
                          "ok",
-                         check_report("Column type is numeric",
+                         check_report("Column type is text",
                                       T,
-                                      "The column %s is numeric", column))
+                                      "The column %s is text", column))
         } else {
             extend_state(state,
                          "continuable",
-                         check_report("Column type is numeric",
+                         check_report("Column type is text",
                                       F,
-                                      "The column %s must be numeric but it appears to be %s instead.", column, class(the_col)));
+                                      "The column %s must be text but it appears to be %s instead.", column, class(the_col)));
         }
     }
 }
 
-#Below function will not work with character columns; have to ensure column passed 'column_is_numeric' before feeding into this
 column_is_integer <- function(column){
     s <- sprintf;
     function(state){
@@ -54,140 +52,6 @@ column_is_integer <- function(column){
                                       "The column %s can only contain integers but it has non-integer values instead. Non-integer values appear at indeces %s.", 
                                       column,
                                       falses))
-        } else {
-            extend_state(state,
-                         "ok",
-                         check_report("Column contains only integers",
-                                      T,
-                                      "The column %s contains only integers", column));
-        }
-    }
-}
-
-
-
-column_in_codelist<-function(column, codelist){
-    function(state){
-        the_col <- state$data[[column]];
-        the_col <- the_col[!is.na(the_col)]
-        check <- the_col %in% codelist
-        wrong <- the_col[!check] %>% unique()
-        if(identical(sum(check), length(check))){
-            extend_state(state,
-                         "ok",
-                         check_report("Column elements in codelist",
-                                      T,
-                                      "The column %s is in the codelist.", column))
-        } else{
-            extend_state(state,
-                         "continuable",
-                         check_report("Column elements in codelist",
-                                      F,
-                                      "The column %s has values that are not in the codelist. These values were not in the codelist: (%s)", 
-                                      column, 
-                                      collapse_commas(wrong)))
-        }
-    }
-}
-
-check_domain_presence <- function(state){
-    data <- state$data;
-    if ("DOMAIN" %in% names(data)){
-        extend_state(state,
-                     "ok",
-                     check_report("DOMAIN column present.",
-                                  T,
-                                  "There is a DOMAIN column."));
-    } else {
-        extend_state(state,
-                     "halted",
-                     check_report("DOMAIN column present.",
-                                  F,
-                                  "There is not a DOMAIN column. Can't check a data set without knowing its DOMAIN."))
-    }
-}
-
-check_domain_homogeneity <- function(state){
-    data <- state$data;
-    if(identical(length(unique(data$DOMAIN)),1)){
-        extend_state(state,
-                     "ok",
-                     check_report("DOMAIN column homogeneous.",
-                                  T,
-                                  "There is only one unique value in the DOMAIN column."))
-    } else {
-        extend_state(state,
-                     "halted",
-                     check_report("DOMAIN column homogeneous.",
-                                  F,
-                                  "DOMAIN column not homogenous. These values found: %s.", collapse_commas(unique(data$DOMAIN))))
-    }
-}
-
-check_domain_known <- function(domains=unique(specification$Datasets$Dataset)){
-    function(state){
-        domain <- unique(state$data$DOMAIN);
-        if(domain %in% domains)
-            extend_state(state, "ok",
-                         check_report("Known DOMAIN", T, "Domain %s is valid.", domain))
-        else extend_state(state, "halted",
-                          check_report("Known DOMAIN", F, "Domain %s is not valid (valid domains %s).",domain,
-                                       collapse_commas(domains)));
-    }
-}
-=======
-column_is_textual <- function(column){
-    s <- sprintf;
-    function(state){
-        the_col <- state$data[[column]];
-        if(class(the_col) %===% class("")){
-            extend_state(state,
-                         "ok",
-                         check_report("Column type is text",
-                                      T,
-                                      "The column %s is text", column))
-        } else {
-            extend_state(state,
-                         "continuable",
-                         check_report("Column type is text",
-                                      F,
-                                      "The column %s must be text but it appears to be %s instead.", column, class(the_col)));
-        }
-    }
-}
-
-column_is_numeric <- function(column){
-    s <- sprintf;
-    function(state){
-        the_col <- state$data[[column]];
-        if(class(the_col)=="numeric"){
-            extend_state(state,
-                         "ok",
-                         check_report("Column type is text",
-                                      T,
-                                      "The column %s is text", column))
-        } else {
-            extend_state(state,
-                         "continuable",
-                         check_report("Column type is text",
-                                      F,
-                                      "The column %s must be text but it appears to be %s instead.", column, class(the_col)));
-        }
-    }
-}
-
-#Below function will not work with character columns; have to ensure column passed 'column_is_numeric' before feeding into this
-column_is_integer <- function(column){
-    s <- sprintf;
-    function(state){
-        the_col <- state$data[[column]];
-        test_vec <- the_col %% 1 == 0
-        if(FALSE %in% test_vec){
-            extend_state(state,
-                         "continuable",
-                         check_report("Column contains only integers",
-                                      F,
-                                      "The column %s can only contain integers but it has non-integer values instead.", column))
         } else {
             extend_state(state,
                          "ok",
@@ -287,4 +151,4 @@ check_domain_known <- function(domains=unique(specification$Datasets$Dataset)){
                                        collapse_commas(domains)));
     }
 }
->>>>>>> b7533b0c256a7312f2ff377edda1c35614c4191c
+
