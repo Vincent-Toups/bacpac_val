99a1dd599ff307d97ca733d5d4d3b305c913ed1e
toups <Vincent Toups>
2021-01-13 13:39:34 -0500

    Added a test case for the ISO8601 duration checker.

--- tests/testthat/test.check_iso8601Duration.R
+++ tests/testthat/test.check_iso8601Duration.R
@@ -39,9 +39,9 @@ gen_random_correct_iso8601_duration <- function(n){
     
 }
 
-test_that("Basic ISO 8601 Duration checks work via the RCPP.",{
+test_that("Basic iso 8601 Duration checks work via the RCPP.",{
     example <- gen_random_correct_iso8601_duration(100);
-    check <- check_ISO8601_durations(example);
+    check <- check_iso8601_durations(example);
     bad_examples = example[!check];
     if(length(bad_examples)!=0){
         print(bad_examples);
@@ -49,9 +49,9 @@ test_that("Basic ISO 8601 Duration checks work via the RCPP.",{
     expect_identical(character(0),bad_examples);
 })
 
-test_that("ISO8601 Duration Checks work with NA values.", {
+test_that("iso8601 Duration Checks work with NA values.", {
     example <- gen_random_correct_iso8601_duration(100);
     ii <- sample(seq(100), 20 , replace=F);
     example[ii] <- NA;
-    expect_identical(rep(NA,20),check_ISO8601_durations(example)[ii]);
+    expect_identical(rep(NA,20),check_iso8601_durations(example)[ii]);
 });
