4258b21c26d0a33073b1d2cfcaffe998ef73daf0
Dan Garbinsky <dangarbinsky@gmail.com>
2021-02-10 15:46:48 -0500

    Added column tests exists, complete, not empty, integer range, homogenous

--- tests/testthat/test.column_tests.R
+++ tests/testthat/test.column_tests.R
@@ -19,7 +19,8 @@ test_dm <- block({
 
 #add a column to test_dm that is non-integer
 test_dm <- test_dm %>% 
-  mutate(non_integer_number=rnorm(150, 0, 1))
+  mutate(non_integer_number=rnorm(150, 0, 1),
+         empty_column=NA)
 
 ## test state functions are pure
 ## and thus we can reuse the fresh dm test state
@@ -64,8 +65,52 @@ test_that("Test that the columns are float and the test function works.",
                              column_is_float("SEX")(dm_state)$status)
           })
 
-test_that("Test that date columns are ok.",{
-    expect_identical("ok",
-                     column_is_iso8601_date("RFSTDTC")(dm_state)$status);
-})
+test_that("Test that date columns are ok.",
+          {
+            expect_identical("ok",
+                            column_is_iso8601_date("RFSTDTC")(dm_state)$status);
+          })
 
+test_that("Test that the columns exist and the test function works.",
+          {
+            expect_identical("ok",
+                             column_exists("USUBJID")(dm_state)$status);
+            expect_identical("continuable",
+                             column_exists("BIRTHDAY")(dm_state)$status)
+          })
+
+test_that("Test that the columns are complete and the test function works.",
+          {
+            expect_identical("ok",
+                             column_is_complete("USUBJID")(dm_state)$status);
+            expect_identical("continuable",
+                             column_is_complete("RACEMULT")(dm_state)$status)
+          })
+
+test_that("Test that the columns are not empty and the test function works.",
+          {
+            expect_identical("ok",
+                             column_not_empty("RACEMULT")(dm_state)$status);
+            expect_identical("continuable",
+                             column_not_empty("empty_column")(dm_state)$status)
+          })
+
+test_that("Test that the columns in integer range and the test function works.",
+          {
+            expect_identical("ok",
+                             column_in_integer_range("AGE", 0:120)(dm_state)$status);
+            expect_identical("continuable",
+                             column_in_integer_range("AGE", 65:70)(dm_state)$status);
+            expect_identical("continuable",
+                             column_in_integer_range("empty_column", 65:70)(dm_state)$status)
+          })
+
+test_that("Test that the columns homogeneous and the test function works.",
+          {
+            expect_identical("ok",
+                             column_is_homogeneous("DOMAIN")(dm_state)$status);
+            expect_identical("ok",
+                             column_is_homogeneous("empty_column")(dm_state)$status);
+            expect_identical("continuable",
+                             column_is_homogeneous("RACE")(dm_state)$status)
+          })
