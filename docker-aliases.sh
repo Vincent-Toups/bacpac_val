3bd772de483bf509a6e474e4933e19b477a2ce66
Vincent <Vincent Toups>
2021-02-18 14:13:28 -0500

    Modified Docker containers, added a separate container for the cli.

--- docker-aliases.sh
+++ docker-aliases.sh
@@ -5,9 +5,10 @@ source secrets.sh
 
 function build_package(){
     TMPD=`mktemp -d`
-    docker run -v $TMPD:/tmp/package-target -v `pwd`:/home/rstudio -e PASSWORD=$RPW -it val-dev sudo -H -u rstudio /bin/bash -c "cd ~/; R -e 'library(devtools); build(path=\"/tmp/package-target\");'"
+    docker run -v $TMPD:/tmp/package-target -v `pwd`:/home/rstudio -e PASSWORD=$RPW -it val-dev sudo -H -u rstudio /bin/bash -c "cd ~/; R -e 'library(devtools); document(); build(path=\"/tmp/package-target\");'"
     FILE=`ls -1 $TMPD/  | head -n 1`
     cp $TMPD/$FILE ./`date | tr ' :' '_' | tr -s '_'`_$FILE
+    cp $TMPD/$FILE ./$FILE
     rm -rf $TMPD
 }
 
@@ -16,4 +17,4 @@ alias dr='docker run -v `pwd`:/home/rstudio -e PASSWORD=$RPW -it val-dev'
 alias r='docker run -v `pwd`:/home/rstudio -e PASSWORD=$RPW -it val-dev sudo -H -u rstudio /bin/bash -c "cd ~/; R"'
 alias b='docker run -v `pwd`:/home/rstudio -e PASSWORD=$RPW -it val-dev sudo -H -u rstudio /bin/bash'
 alias rss='docker run -p 8787:8787 -v `pwd`:/home/rstudio -e PASSWORD=$RPW -t val-dev'
-
+alias dev='x11docker --clipboard --share ~/.ssh --share ~/.gitconfig --share ~/.emacs.d --share `pwd` val-dev xfce4-terminal'
