35cdb840af3b8749e5129413af6ddefb69cabbbd
Vincent Toups <Vincent Toups>
2021-07-02 15:56:08 -0400

    Added option to return only failed tests to CLI.

--- scripts/cli.R
+++ scripts/cli.R
@@ -2,12 +2,20 @@
 
 library(val);
 library(readr);
+library(tidyverse);
 
 args <- commandArgs(trailingOnly=TRUE);
 
 print_usage <- function(){
     write(sprintf("bacpac_validate - input-file output-file"))
 }
+
+brief <- if(length(args)>=1 && (args[[1]] == "-b"||args[[1]] == "--brief")){
+             TRUE;
+         } else {
+             args <- args[2:(length(args)-1)];
+             FALSE;
+         };
 
 if(length(args)!=2 | !file.exists(args[[1]])){
     print_usage();
@@ -25,4 +33,12 @@ data <- val_read_csv(input);
 
 result <- validate_generic(data);
 
-readr::write_csv(result$messages, output);
+out <- result$messages;
+
+if(brief){
+    out <- out %>% filter(pass==FALSE);
+} else {
+    out <- out;
+}
+
+readr::write_csv(out, output);
